bb897e307474e5dfa3c251b389f191d70026e69f
gloder_cp <SND\gloder_cp>
2013-01-22 18:35:32 -0800

    Fixed a confusing bug that made its appearance today. After debugging for a while, the extension menus were no longer showing up. After a day of research I finally determined that an attribute was missing in the vsix package file. Also added a logo file, and modified the configuration to create only x86 assemblies for debugging.

--- XamlHelpmeet/XamlHelpmeetPackage.cs
+++ XamlHelpmeet/XamlHelpmeetPackage.cs
@@ -36,6 +36,7 @@ namespace XamlHelpmeet
 	// This attribute is used to register the information needed to show this package
 	// in the Help/About dialog of Visual Studio.
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[Guid(GuidList.guidXamlHelpmeetPkgString)]
 	public sealed class XamlHelpmeetPackage : Package
 	{
